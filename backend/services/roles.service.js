274cf2e46c50febb2dfc0d462633c1056c25e387
Vaibhav <dev.vaibhav99@gmail.com>
2023-10-06 11:05:24 +0530

    roles service added

---
+++ backend/services/roles.service.js
@@ -0,0 +1,17 @@
+var roleDao = require("../dao/role.dao");
+
+const getAllRoles = async () => {
+  let rows = [];
+  await roleDao
+    .getAllRoles()
+    .then((res) => {
+      rows = res;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+  return rows;
+};
+
+module.exports = { getAllRoles };
