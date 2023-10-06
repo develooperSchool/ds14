0b057b0ae34d7176ca230b4ada9e4ff7ef0b882c
NehaS6855 <nehasapte1995@gmail.com>
2023-10-06 10:44:30 +0530

    changes done

--- backend/middlewares/validations/salaryInfo.validation.js
+++
@@ -1,8 +0,0 @@
-const utils = require('../../utils/app.utils');
-
-const getSalaryInfoByIdValidation = (req,res,next)=>{
-    if(utils.isValidId(req.params.id)) res.status(400).send("ID IS INVALID");
-    else next();
-}
-
-module.exports={getSalaryInfoByIdValidation};
