1fd20a2e49b0c8d6152b089f0eea6228a7ea1f08
Vaibhav <dev.vaibhav99@gmail.com>
2023-10-06 10:31:59 +0530

    validation added for one api

--- backend/routes/course.routes.js
+++ backend/routes/course.routes.js
@@ -1,22 +1,30 @@
 const express = require("express");
 const router = express.Router();
 const courseController = require("../controllers/course.controller");
-
+const courseValidation = require("../middlewares/validations/course.validation");
+const { valid } = require("@hapi/joi");
+// api/v1/course/byName?name=FRONTEND                 -- QUERY PARAM   - req.query.name
+// api/v1/course/:name --> api/v1/course/FRONTEND     -- PATH VARIABLE - req.params.name
+//
 const courseCon =
-  // api/v1/course
+  // api/v1/course/
   router.get("/", (req, res) => {
     courseController.getAllCourses(req, res);
   });
 
 // /api/v1/course/1
-router.get("/:id", (req, res) => {
-  courseController.getCourseById(req, res);
-});
+router.get(
+  "/:id",
+  courseValidation.getCourseByIdValidation,
+  courseController.getCourseById
+);
 
 // api/v1/course/add
-router.post("/add", (req, res) => {
-  courseController.addCourse(req, res);
-});
+router.post(
+  "/add",
+  courseValidation.addCourseValidation,
+  courseController.addCourse
+);
 
 router.put("/:id", (req, res) => {
   courseController.updateCourse(req, res);
