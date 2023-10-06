913fa98c8d5c083c5427e1227a442cee45ec6226
Vaibhav <dev.vaibhav99@gmail.com>
2023-10-06 10:59:38 +0530

    changes made to merge user branch

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,13 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users.routes");
 var revenueRouter = require("./routes/revenue.routes");
-var roleRouter=require("./routes/roles.routes")
+var roleRouter = require("./routes/roles.routes");
 var courseRoutes = require("./routes/course.routes");
 var app = express();
 
@@ -22,7 +22,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
 app.use("/api/v1/revenue", revenueRouter);
 
 app.use("/api/v1/roles", roleRouter);
@@ -31,19 +30,19 @@ app.use("/api/v1/course", courseRoutes);
 // https://localhost:3000/api/v1/course/
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
