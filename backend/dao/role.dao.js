c774e4c14decdc8ee469a7d06ee2bbb2b630b706
Vaibhav <dev.vaibhav99@gmail.com>
2023-10-06 11:03:24 +0530

    role dao file added

---
+++ backend/dao/role.dao.js
@@ -0,0 +1,14 @@
+var pool = require("../config/db-config");
+
+const getAllRoles = async () => {
+  let row = [];
+  try {
+    const [rows] = await pool.query("SELECT * FROM userRole");
+    row = rows;
+  } catch (err) {
+    console.error(err);
+  }
+  return row;
+};
+
+module.exports = { getAllRoles };
