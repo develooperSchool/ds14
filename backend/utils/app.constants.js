6332f8e93fbd821f03fbd3365895b30e582a1850
devsurajgawai <suraj.gawai@careerinfotechsolution.in>
2023-10-10 17:21:58 +0530

    final constant added

--- backend/utils/app.constants.js
+++ backend/utils/app.constants.js
@@ -10,6 +10,34 @@ const INVALID_SUBCASTE = "INVALID SUBCASTE";
 const INVALID_QUALIFICATION = "INVALID QUALIFICATION";
 const INVALID_DATE = "INVALID DATE";
 const INVALID_PASSWORD = "INVALID PASSWORD";
+//constants for course module.......................
+const COURSE_ID1 = "INVALID COURSE ID";
+const COURSE_NAME1 = "INVALID COURSE NAME";
+const COURSE_DURATION1 = "INVALID COURSE DURATION";
+const COURSE_FEES1 = "INVALID COURSE FEES";
+
+//enrollment constants.....................
+const UNIQUE_ID2 = "INVALID UNIQUE ID";
+const USER_ID2 = "INVALID USER ID";
+const COURSE_ID2 = "INVALID COURSE ID";
+
+//faculty2 constants...................
+const sub_id3 = " INVALID SUBJECT ID";
+const user_id3 = "INVALID USER ID";
+const faculty_id3 = "INVALID FACULTY ID";
+
+//subjects constants...................
+const sub_id4 = "INVALID SUBJECT ID";
+const subject4 = " INVALID SUBJECT NAME";
+const duration4 = " INVALID DURATION";
+const faculty_id4 = " INVALID FACULTY ID";
+const syllabus4 = "INVALID SYLLABUS LINK";
+
+//timetable constants.....................
+const id5 = " INVALID ID";
+const sub_id5 = " INVALID SUBJECT ID";
+const time5 = " INVALID TIME FIELD";
+const faculty_id5 = " INVALID FACULTY ID";
 module.exports = {
   INVALID_NAME,
   INVALID_ID,
@@ -23,4 +51,27 @@ module.exports = {
   INVALID_DATE,
   INVALID_PASSWORD,
   INVALID_QUALIFICATION,
+  COURSE_DURATION1,
+  COURSE_FEES1,
+  COURSE_ID1,
+  COURSE_NAME1,
+  //enrollment module constants...................
+  UNIQUE_ID2,
+  USER_ID2,
+  COURSE_ID2,
+  //facullty2 constants...................
+  sub_id3,
+  user_id3,
+  faculty_id3,
+  //subjects constants.......................
+  sub_id4,
+  subject4,
+  duration4,
+  faculty_id4,
+  syllabus4,
+  //timetableconstants.....................
+  id5,
+  sub_id5,
+  time5,
+  faculty_id5,
 };
