66bd8dd33ea38f400bef86cac403e807e19b40e0
NehaS6855 <nehasapte1995@gmail.com>
2023-10-11 23:11:36 +0530

    Changes Done in feature/hr_payroll

--- backend/controllers/salaryInfoController.js
+++ backend/controllers/salaryInfoController.js
@@ -1,9 +1,10 @@
 const salaryInfoService = require("../services/salaryInfoService");
+const HttpStatusCode = require("../utils/HttpStatusCode");
 
 async function getAllSalaryInfo(req, res, next) {
   try {
     const salaryInfo = await salaryInfoService.getAllSalaryInfo(res);
-    res.json(salaryInfo);
+    res.status(HttpStatusCode.OK).json(salaryInfo);
   } catch (error) {
     next(error);
   }
@@ -13,7 +14,7 @@ async function getSalaryInfoById(req, res, next) {
   try {
     const salaryId = req.params.salary_id;
     const salaryInfo = await salaryInfoService.getSalaryInfoById(salaryId, res);
-    res.json(salaryInfo);
+    res.status(HttpStatusCode.OK).json(salaryInfo);
   } catch (error) {
     next(error);
   }
@@ -26,7 +27,7 @@ async function addSalaryInfo(req, res, next) {
       newSalaryInfo,
       res
     );
-    res.status(201).json({
+    res.status(HttpStatusCode.CREATED).json({
       message: "Salary information added successfully",
       id: insertedId,
     });
@@ -40,7 +41,9 @@ async function updateSalaryInfo(req, res, next) {
     const salaryId = req.params.salary_id;
     const updatedSalaryInfo = req.body;
     await salaryInfoService.updateSalaryInfo(salaryId, updatedSalaryInfo, res);
-    res.json({ message: "Salary information updated" });
+    res
+      .status(HttpStatusCode.OK)
+      .json({ message: "Salary information updated" });
   } catch (error) {
     next(error);
   }
@@ -50,7 +53,9 @@ async function deleteSalaryInfo(req, res) {
   try {
     const salaryId = req.params.salary_id;
     await salaryInfoService.deleteSalaryInfo(salaryId, res);
-    res.json({ message: "Salary information deleted successfully" });
+    res
+      .status(HttpStatusCode.OK)
+      .json({ message: "Salary information deleted successfully" });
   } catch (error) {
     next(error);
   }
