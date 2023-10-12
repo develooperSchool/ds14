681b7a1d6dfc85aa8c718feaf92152f38ffa2bf3
Mksadar <mksadar12@gmail.com>
2023-10-12 20:12:36 +0530

    review correction

--- backend/controllers/student.controller.js
+++ backend/controllers/student.controller.js
@@ -1,10 +1,11 @@
 const service = require("../services/student.service");
+const httpStatusCode = require("../utils/HttpStatusCode");
 
 const getAllStudents = (req, res) => {
   service
     .getAllStudents(req, res)
     .then((resp) => {
-      res.status(200).json(resp);
+      res.status(httpStatusCode.OK).json(resp);
     })
     .catch((err) => {
       console.log(err);
