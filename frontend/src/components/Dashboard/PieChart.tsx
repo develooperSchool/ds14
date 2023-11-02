009a5bb8f19b0865f159c6ac73603014ce37fce8
devsurajgawai <suraj.gawai@careerinfotechsolution.in>
2023-11-02 15:10:28 +0530

    dashboard added

---
+++ frontend/src/components/Dashboard/PieChart.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PieChart = () => {
+  return (
+    <>
+      <h3>Pie Chart</h3>
+    </>
+  );
+};
+
+export default PieChart;
---
+++ frontend/src/components/Dashboard/Sidebar.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Sidebar = () => {
+  return (
+    <>
+      <div className="navsidebar text-start bg-secondary h-100">
+        <ul className="list-group d-flex flex-column">
+          <li className="mt-5">
+            <Link to={"/"}>
+              <i className="bi bi-person-fill-lock text-dark">Admin</i>
+            </Link>
+          </li>
+          <li className="mt-3">
+            <Link to={"#"}>
+              <i className="bi bi-person-fill text-dark">Mentor</i>
+            </Link>
+          </li>
+          <li className="mt-3">
+            <Link to={"#"}>
+              <i className="bi-person-lines-fill text-dark">Student</i>
+            </Link>
+          </li>
+          <li className="mt-3">
+            <Link to={"/course"}>
+              <i className=" bi bi-book-half text-dark">Cource</i>
+            </Link>
+          </li>
+          <li className="mt-3">
+            <Link to={"#"}>
+              <i className="bi bi-file-text-fill text-dark">Carriculam</i>
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default Sidebar;
