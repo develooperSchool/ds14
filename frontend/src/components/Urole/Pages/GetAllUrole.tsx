4748eca35fa9ba17416918a656b64d679388149c
Rahulrindhe <rindherahul23@gmail.com>
2023-12-05 00:41:53 +0530

    searching operation

--- frontend/src/components/Urole/Pages/GetAllUrole.tsx
+++ frontend/src/components/Urole/Pages/GetAllUrole.tsx
@@ -1,20 +1,44 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import * as UroleReducer from "../../../Redux/UroleRedux/urole.reducer";
 import * as UroleAction from "../../../Redux/UroleRedux/urole.action";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "../../../Redux/store";
 import { Link } from "react-router-dom";
 import { usePagination } from "../../Pagination";
-import { Pagination } from "react-bootstrap";
+import { Col, Pagination } from "react-bootstrap";
+import { Iurole } from "../Model/Iurole";
+import axios from "axios";
 
 const GetUrole: React.FC = () => {
+  ///filtering and sortig code
+  const [data, setData] = useState<Iurole[]>([]);
+  const [sortedData, setSortedData] = useState("ASC");
+  const [search, setSearch] = useState("");
+  // const [filteredData, setFilteredData] = useState<Iurole[]>([]);
+  // const [filterText, setFilterText] = useState<string>("");
+
   //data from redux store
   const uroleReduxState: UroleReducer.InitialState = useSelector(
     (state: RootState) => {
       return state[UroleReducer.uroleFeatureKey];
     }
   );
+
   const dispatach: AppDispatch = useDispatch();
+
+  ///code for searching
+  const searchItem = uroleReduxState.uroles.filter((item) => {
+    if (search == "") {
+      return item;
+    } else if (
+      item.role_id.toString().includes(search) ||
+      item.role_name.toLowerCase().includes(search.toLowerCase())
+    ) {
+      return item;
+    }
+  });
+
+  ////pagination code
   const {
     totalPages,
     startPageIndex,
@@ -22,7 +46,7 @@ const GetUrole: React.FC = () => {
     currentPageIndex,
     displayPage,
   } = usePagination({
-    perPageRecords: 5,
+    perPageRecords: 6,
     totalPageRecords: uroleReduxState.uroles.length,
   });
 
@@ -31,18 +55,22 @@ const GetUrole: React.FC = () => {
     console.log(uroleReduxState);
   }, []);
 
+  //
+
+ 
+
+  
+
   const dataFromserver = () => {
     dispatach(UroleAction.getAllRolesAction());
   };
   const deleteRole = (id: string) => {
     if (id) {
-      dispatach(UroleAction.deleteRoleAction({ id: id })).then(
-        (res: any) => {
-          if (res && !res.error) {
-            dataFromserver();
-          }
+      dispatach(UroleAction.deleteRoleAction({ id: id })).then((res: any) => {
+        if (res && !res.error) {
+          dataFromserver();
         }
-      );
+      });
     }
   };
 
@@ -63,9 +91,30 @@ const GetUrole: React.FC = () => {
           </div>
         </div>
       </div>
-      <Link to="/addurole" className="btn btn-outline-info">
-        Add New Role Here
-      </Link>
+
+      <div>
+        <div className="row">
+          <div className="col">
+            <Link to="/addurole" className="btn btn-outline-info">
+              Add New Role Here
+            </Link>
+          </div>
+          <div className="col-3">
+            <input
+              type="text"
+              placeholder="Search Here"
+              className="form-control"
+              onChange={(event) => setSearch(event.target.value)}
+            />
+          </div>
+        </div>
+      </div>
+
+     
+
+      
+      
+        
 
       <div className="container">
         <div className="row">
@@ -79,35 +128,34 @@ const GetUrole: React.FC = () => {
                 </tr>
               </thead>
               <tbody>
-              {uroleReduxState.uroles.slice(
-                  startPageIndex,
-                  endPageIndex + 1
-                ).map((urole: any) => {
-                  return (
-                    <tr>
-                      <td>{urole.role_id}</td>
-                      <td>{urole.role_name}</td>
-                      <td>
-                        <Link
-                          to={`/updateurole/${urole.role_id}`}
-                          className="btn btn-outline-success"
-                        >
-                          Update
-                        </Link>
-                        <button
-                          className="btn btn-outline-danger"
-                          onClick={() =>
-                            deleteRole(
-                              urole.role_id
-                            )
-                          }
-                        >
-                          Delete
-                        </button>
-                      </td>
-                    </tr>
-                  );
-                })}
+                {/* {uroleReduxState.uroles
+                  .slice(startPageIndex, endPageIndex + 1) */}
+                {(searchItem.length > 0 ? searchItem : uroleReduxState.uroles)
+                
+                
+                  .slice(startPageIndex, endPageIndex + 1)
+                  .map((urole: any) => {
+                    return (
+                      <tr>
+                        <td>{urole.role_id}</td>
+                        <td>{urole.role_name}</td>
+                        <td>
+                          <Link
+                            to={`/updateurole/${urole.role_id}`}
+                            className="btn btn-outline-success"
+                          >
+                            Update
+                          </Link>
+                          <button
+                            className="btn btn-outline-danger"
+                            onClick={() => deleteRole(urole.role_id)}
+                          >
+                            Delete
+                          </button>
+                        </td>
+                      </tr>
+                    );
+                  })}
               </tbody>
             </table>
             <Pagination>
