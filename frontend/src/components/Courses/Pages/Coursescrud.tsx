cabe20f065f4ab8f0241d2bfcf9ca47d16df2934
musaib_qureshi <musaif23@gmail.com>
2023-12-05 08:42:53 +0530

    pull for update

--- frontend/src/components/Courses/Pages/Coursescrud.tsx
+++ frontend/src/components/Courses/Pages/Coursescrud.tsx
@@ -7,6 +7,8 @@ import { useDispatch } from "react-redux";
 import { ICOURSES } from "../Model/Icourses";
 import "../../../style/Courses/courescrud.css";
 import { Link } from "react-router-dom";
+import Pagination from "react-bootstrap/esm/Pagination";
+import { usePagination } from "../../Pagination";
 
 let Coursescrud: React.FC = () => {
   let courseReduxState: courseReducer.initialState = useSelector(
@@ -16,6 +18,16 @@ let Coursescrud: React.FC = () => {
   );
 
   let dispatch: AppDispatch = useDispatch();
+  const {
+    totalPages,
+    startPageIndex,
+    endPageIndex,
+    currentPageIndex,
+    displayPage,
+  } = usePagination({
+    perPageRecords: 5,
+    totalPageRecords: courseReduxState.courses.length,
+  });
 
   let dataFromServer = () => {
     dispatch(courseActions.getAllCourseAction());
@@ -50,36 +62,61 @@ let Coursescrud: React.FC = () => {
             </tr>
           </thead>
           <tbody>
-            {courseReduxState.courses.map((elem: ICOURSES, ind: any) => {
-              return (
-                <tr className="tr">
-                  <th scope="row">{ind + 1}</th>
-                  <td>{elem.course_name}</td>
-                  <td>{elem.course_duration}</td>
-                  <td>{elem.course_fees}</td>
-                  <td>
-                    <div className="course_action_button">
-                      <Link
-                        to={`/course_update/${elem.course_id}`}
-                        className="btn btn-outline-warning"
-                      >
-                        Update
-                      </Link>
-                      <button
-                        className="btn btn-outline-danger"
-                        onClick={() => {
-                          deleteCourse(elem.course_id);
-                        }}
-                      >
-                        Delete
-                      </button>
-                    </div>
-                  </td>
-                </tr>
-              );
-            })}
+            {courseReduxState.courses
+              .slice(startPageIndex, endPageIndex + 1)
+              .map((elem: ICOURSES, ind: any) => {
+                return (
+                  <tr className="tr">
+                    <th scope="row">{ind + 1}</th>
+                    <td>{elem.course_name}</td>
+                    <td>{elem.course_duration}</td>
+                    <td>{elem.course_fees}</td>
+                    <td>
+                      <div className="course_action_button">
+                        <Link
+                          to={`/course_update/${elem.course_id}`}
+                          className="btn btn-outline-warning"
+                        >
+                          Update
+                        </Link>
+                        <button
+                          className="btn btn-outline-danger"
+                          onClick={() => {
+                            deleteCourse(elem.course_id);
+                          }}
+                        >
+                          Delete
+                        </button>
+                      </div>
+                    </td>
+                  </tr>
+                );
+              })}
           </tbody>
         </table>
+        <div>
+          <Pagination>
+            <Pagination.First onClick={() => displayPage(1)} />
+            <Pagination.Prev
+              onClick={() => displayPage(currentPageIndex - 1)}
+              disabled={currentPageIndex === 1}
+            />
+            {[...Array(totalPages)].map((_, index) => (
+              <Pagination.Item
+                key={index + 1}
+                active={index + 1 === currentPageIndex}
+                onClick={() => displayPage(index + 1)}
+              >
+                {index + 1}
+              </Pagination.Item>
+            ))}
+            <Pagination.Next
+              onClick={() => displayPage(currentPageIndex + 1)}
+              disabled={currentPageIndex === totalPages}
+            />
+            <Pagination.Last onClick={() => displayPage(totalPages)} />
+          </Pagination>
+        </div>
       </div>
     </>
   );
