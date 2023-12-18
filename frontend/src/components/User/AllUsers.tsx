e8cc7b6feb8c7ecc48885627ff58dc70cdcc46a3
Mksadar <mksadar12@gmail.com>
2023-12-18 21:13:54 +0530

    conflicts

--- frontend/src/components/User/AllUsers.tsx
+++ frontend/src/components/User/AllUsers.tsx
@@ -48,7 +48,6 @@ const AllUsers: React.FC = () => {
       item.qualification.toLowerCase().includes(search.toLowerCase()) ||
       item.user_id?.toString().includes(search)
     ) {
-      console.log("item", item.first_name.toLowerCase());
       return item;
     }
   });
