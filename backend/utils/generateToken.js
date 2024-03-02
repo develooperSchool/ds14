222e66cd8828c838ff98ec1943f01cbc10e615d3
Vaibhav <dev.vaibhav99@gmail.com>
2024-03-02 14:58:15 +0530

    JWT secret is now changed to secret

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
 
 dotenv.config();
-const JWT_SECRET = process.env.JWT_SECRET;
+const JWT_SECRET = "secret";
 
 const generateToken = (body) => {
   const email = body.email;
